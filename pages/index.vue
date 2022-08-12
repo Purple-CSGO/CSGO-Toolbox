d8a57f2262231fa0bd314d61292e4d6a1b62c98e
Purp1e <47248616+Purple-CSGO@users.noreply.github.com>
2022-08-12 17:44:14 +0800

    页面布局完成 版本号获取待添加

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,85 @@
 <template>
-	<div class="p-4 rounded-xl flex content-center items-center justify-center w-full h-[100vh] bg-from-[rgba(224,115,115,0.62)]" >
-		hello world
+	<div class="bg">
+		<img src="/logo.png" class="logo" >
+		
+		<div class=" relative" >
+			<p class="title" >CSGO工具箱</p>
+			<span class="badge" >Beta</span>
+		</div>
+		<p class="slogan" >一个给CSGO游戏的各个方面带来便利的工具集合</p>
+		
+		<div class="btn-group" >
+			<button class="btn-download"  >
+				<a :href="download_url" target="_blank">下载</a>
+			</button>
+			<button class="btn-repo" >
+				<img src="/github-fill.svg" class="w-5" alt="" >
+				<a href="https://github.com/One-Studio/CSGO-Toolbox" target="_blank" class="font-bold text-[#555]" >项目</a>
+			</button>
+		</div>
+		
+		<p class="text-xs opacity-70 tracking-wider" >当前版本：<span>{{version}}</span></p>
 	</div>
 </template>
+
+<script setup >
+// "https://api.upup.cool/repo/One-Studio/CSGO-Toolbox/version"
+const version = "0.0.9-alpha.11"
+const download_url = "https://api.upup.cool/repo/One-Studio/CSGO-Toolbox/&amd64&&&.exe"
+
+</script>
+
+<style>
+* {
+	font-family: -apple-system, "PingFang SC", "Microsoft YaHei", system-ui, Arial, serif;
+}
+
+
+[v-cloak]{
+	display: none !important;
+	transition: none;
+}
+</style>
+
+<style scoped>
+.bg {
+	@apply w-[100vw] h-[100vh] p-4 flex flex-col content-center items-center justify-center text-center;
+	background: linear-gradient(270deg, rgba(233,181,181,0.62), #F2EAF1);
+}
+
+.logo {
+	@apply w-36 shadow-lg m-8;
+	border-radius: 32px;
+}
+
+.title {
+	@apply font-bold opacity-80;
+	font-size: 56px;
+	line-height: 64px;
+}
+
+.badge {
+	@apply rounded-full bg-white bg-opacity-80 text-[#555] font-normal absolute px-2 py-0.5 -top-1 -right-12;
+	font-size: 10px;
+	line-height: 18px;
+}
+
+.slogan {
+	@apply opacity-60 m-3 tracking-widest;
+	font-size: 18px;
+	line-height: 26px;
+}
+
+.btn-group {
+	@apply flex gap-2 mt-6 mb-4;
+}
+
+.btn-download {
+	@apply p-3 w-32 rounded-full text-sm shadow-sm hover:shadow-md hover:bg-[#E87B5C] active:scale-95 transition duration-300 bg-[#FF9071] text-white font-semibold tracking-wider;
+}
+
+.btn-repo {
+	@apply py-3 px-6 rounded-full text-sm shadow-sm hover:shadow-md active:scale-95 transition duration-300 bg-white bg-opacity-70 flex justify-center items-center gap-1;
+}
+</style>
 
